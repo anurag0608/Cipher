5490d1abed9befb953a17a4b761dc323a4ec04b5
anurag <anuragv.1020@gmail.com>
2021-05-06 16:29:38 +0530

    rail fence cipher decryption done!

--- RailFence/src/com/railfence/RailFenceCipher.java
+++ RailFence/src/com/railfence/RailFenceCipher.java
@@ -6,6 +6,8 @@ class RailFence{
 
     }
     public String encrypt(String plaintext){
+        if(this.key==1)
+            return plaintext;
         // allocating memory
         StringBuilder[] railFence = new StringBuilder[this.key];
         for (int i = 0; i < this.key; i++)
@@ -38,11 +40,59 @@ class RailFence{
         }
         return enc.toString();
     }
+    public String decrypt(String cipherText){
+        StringBuilder plaintext = new StringBuilder();
+        int j = 0;
+        int row = this.key;
+        int col = cipherText.length();
+        boolean down = false;
+        char[][]  mat = new char[row][col];
+        for(int i=0;i<col;i++){
+            if(j==0 || j==this.key-1){
+                down = !down;
+            }
+            mat[j][i] = '*';
+            if(down)
+                j++;
+            else
+                j--;
+        }
+        int index = 0;
+        // replace '*' with ciphertext alphabets row wise
+        for(int i=0;i<row;i++){
+            for(int k=0;k<col;k++){
+                if(mat[i][k]=='*' && index<col)
+                    mat[i][k] = cipherText.charAt(index++);
+            }
+        }
+        // printing key matrix
+//        for(int i=0;i<row;i++){
+//            for(int k=0;k<col;k++){
+//                System.out.print(mat[i][k]+" ");
+//            }
+//            System.out.println();
+//        }
+        // build the final string
+        j=0;
+        down = false;
+        for(int i=0;i<col;i++){
+            if(j==0 || j==this.key-1){
+                down = !down;
+            }
+            plaintext.append(mat[j][i]);
+            if(down)
+                j++;
+            else
+                j--;
+        }
+        return plaintext.toString();
+    }
 }
 public class RailFenceCipher {
     public static void main(String[] args) {
-        RailFence rf = new RailFence(2);
-        String enc = rf.encrypt("my name is Anurag!");
-        System.out.println(enc);
+        RailFence rf = new RailFence(4);
+        String enc = rf.encrypt("hello my name is Anurag!! :)");
+        System.out.println("Encrypted Text: "+enc);
+        System.out.println("Decrypyed Text: "+rf.decrypt(enc));
     }
 }
